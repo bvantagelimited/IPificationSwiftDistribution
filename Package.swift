aff9d4c4a06588c76844e92b53cbb0b551233fc3
tinhnguyen <tn@ipification>
2021-01-07 09:49:14 +0700

    Added package specification

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.3
+import PackageDescription
+let package = Package(
+    name: "IPificationFramework",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "IPificationFramework", 
+            targets: ["IPificationFramework"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "IPificationFramework", 
+            path: "IPificationSDKFramework.xcframework")
+    ])
