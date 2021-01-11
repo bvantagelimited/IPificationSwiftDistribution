2232015f43b06bec202a96a70032837e1a5a1e82
tinhnguyen <tn@ipification>
2021-01-11 14:06:47 +0700

    correct the min ios version

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "IPificationFramework",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v10)
     ],
     products: [
         .library(
