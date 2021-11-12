449fa58734260b0cd3b22f6704dd3ee1fd6b8d15
tinhnguyen <tn@ipification>
2021-11-12 17:04:40 +0700

    release 1.8

---
+++ IPificationSDK.xcframework/ios-arm64_i386_x86_64-simulator/IPificationSDK.framework/Headers/IPificationSDK.h
@@ -0,0 +1,19 @@
+//
+//  IPificationSDK.h
+//  IPificationSDK
+//
+//  Created by IPification Mobile on 26/5/2020.
+//  Copyright © 2020 IPification Mobile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IPificationSDK.
+FOUNDATION_EXPORT double IPificationSDKVersionNumber;
+
+//! Project version string for IPificationSDK.
+FOUNDATION_EXPORT const unsigned char IPificationSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IPificationSDK/PublicHeader.h>
+
+
